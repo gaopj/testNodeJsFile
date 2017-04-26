14a74676922b667c5e22cec2c937c6128380161f
gaopiji <448030133@qq.com>
2017-04-26 21:25:11 +0800

    完成Cookie实验

--- cookie/index.js
+++ cookie/index.js
@@ -12,11 +12,11 @@ app.get('/gpj',function(req,res,next){
 	res.json(req.cookies);
 });
 app.get('/write',function(req,res,next){
-	res.cookie('my_cookie','hello',{
+	//res.cookie('my_cookie','hello',{
 	//domain:"www.gaopiji.cn",path:'/',
 	//expires:new Date(Date.now()+1*60*1000)
-	maxAge:1*60*1000 //
-	});//一分钟后过期
+	//maxAge:1*60*1000 //
+	//});//一分钟后过期
 
 	res.cookie('a','123');
 	res.cookie('b','456',{httpOnly:true,signed:true});
